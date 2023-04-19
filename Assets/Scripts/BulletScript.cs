255a8404d227f9ff171d979bc41d43740400b008
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-04-19 11:38:27 +0300

    Created prototype shooting system

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private LayerMask _obstacleLayer;
+
+    public LayerMask TargetLayer;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rigidbody.velocity = transform.TransformDirection(Vector2.up * _speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log((int)_obstacleLayer);
+        if ((TargetLayer & (1 << collision.gameObject.layer)) != 0)
+        {
+            Destroy(collision.gameObject);
+            if (collision.gameObject.GetComponent<CharacterMovement>())
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            Destroy(gameObject);
+        }
+        else if ((_obstacleLayer & (1 << collision.gameObject.layer)) != 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
