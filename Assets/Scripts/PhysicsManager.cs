73ae8f676309a01db39def5c84f39015bdaa827a
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-04-27 13:44:22 +0300

    Added black, grass pathwalk tiles
    Black tiles cover grass out of field of view

--- Assets/Scripts/PhysicsManager.cs
+++ Assets/Scripts/PhysicsManager.cs
@@ -6,10 +6,12 @@ public class PhysicsManager : MonoBehaviour
 {
     [SerializeField] private int _bulletLayer;
     [SerializeField] private int _enemyLayer;
+    [SerializeField] private int _wallLayer;
 
     void Start()
     {
         Physics2D.IgnoreLayerCollision(_bulletLayer, _bulletLayer);
         Physics2D.IgnoreLayerCollision(_enemyLayer, _enemyLayer);
+        Physics2D.IgnoreLayerCollision(_wallLayer, _wallLayer);
     }
 }
